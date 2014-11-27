75c05886daafc6d5609c784f963ef619d74dae9c
Annabel <annabel.menard@gmail.com>
2014-11-27 18:39:32 +0100

    auto-completion

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,7 @@
 class School < ActiveRecord::Base
+  include AlgoliaSearch
+  algoliasearch do
+    attributesToIndex ['address', 'name', 'city']
+    # associated index settings can be configured from here
+  end
 end
