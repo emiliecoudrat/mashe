2c1eb4198b2f5037b8f227b66b1d66bdc751616e
CandiceSG <candice.salagasperini@gmail.com>
2014-11-27 15:40:07 +0100

    generate controller camp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'camp/new'
+
+  get 'camp/create'
+
+  get 'camp/index'
+
+  get 'camp/show'
+
+  get 'camp/edit'
+
+  get 'camp/update'
+
+  get 'camp/destroy'
+
   ActiveAdmin.routes(self)
 
   devise_for :parents
