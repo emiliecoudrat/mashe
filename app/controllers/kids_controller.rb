76ed90ca12a9503a68289e25d0ed477e7b396cbe
CandiceSG <candice.salagasperini@gmail.com>
2014-11-27 12:48:41 +0100

    tableau listing kid en cours

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,10 +1,23 @@
 class KidsController < ApplicationController
+  before_action :set_kid, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_parent!, only: :index
 
   def new
+    @kid = Kid.new
+    respond_with(@kid)
   end
 
   def create
+    @kid = Kid.new(kid_params)
+    @kid.save
+    respond_with(@kid)
+  end
+
+  def index
+    @kids = Kid.all.camp_id
+  end
+
+  def show
   end
 
   def update
@@ -16,9 +29,13 @@ class KidsController < ApplicationController
   def destroy
   end
 
-  def index
+private
+  def set_kid
+    @kid = Kid.find(params[:id])
   end
 
-  def show
+  def kid_params
+    params.require(:kid).permit(:firstname, :lastname, :birthdate, :gender)
   end
+
 end
