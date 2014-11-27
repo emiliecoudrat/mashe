cf14a57694c0f2869312e68382f15bb14f317b21
Caroline Carriere <caroline.carrierek@gmail.com>
2014-11-27 14:38:45 +0100

    add-scholarship-model

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -1,8 +1,7 @@
 class Camp < ActiveRecord::Base
   belongs_to :level
   belongs_to :school
-  has_many :scholarship
-end
+  has_many :scholarships
 
   validates_presence_of :name, :confidential_code, :level_id, :school_id
 end
