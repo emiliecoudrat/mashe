bd25ccdfdbd2df40370f6c7d6abad0f0deb864f6
CandiceSG <candice.salagasperini@gmail.com>
2014-11-28 13:30:49 +0100

    camp controller remis à zero

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,11 @@
 Rails.application.routes.draw do
-  get 'camp/new'
 
-  get 'camp/create'
-
-  get 'camp/index'
-
-  get 'camp/show'
-
-  get 'camp/edit'
-
-  get 'camp/update'
-
-  get 'camp/destroy'
 
   ActiveAdmin.routes(self)
 
   devise_for :parents
   root to: "home#index"
-
+  resources :camps
   resources :kids do
     resources :parentships
   end
