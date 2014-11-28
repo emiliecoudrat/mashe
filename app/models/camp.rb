d2adb887fbecc0597bc86c640c3248c96fd63655
Caroline Carriere <caroline.carrierek@gmail.com>
2014-11-28 15:57:49 +0100

    news_filters_active_admin

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -4,4 +4,8 @@ class Camp < ActiveRecord::Base
   has_many :scholarships
 
   validates_presence_of :name, :confidential_code, :level_id, :school_id
+
+  def display_name
+    "#{school.name} - #{level.name} - #{name} - #{year}"
+  end
 end
