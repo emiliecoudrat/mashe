13cee5d0ee9e5e3e0543e414bddc2b74ec8b0ce7
Caroline Carriere <caroline.carrierek@gmail.com>
2014-11-29 11:17:37 +0100

    advert-category-transaction-for-index

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -1,9 +1,52 @@
 class AdvertsController < InheritedResources::Base
+  before_action :set_advert, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  before_action :set_school
+  skip_before_action :authenticate_parent!, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
-  private
+  def index
+    @adverts = @school.adverts.all
+  end
 
-    def advert_params
-      params.require(:advert).permit(:title, :description, :category, :transaction, :price_cents, :published, :sold, :parent_id, :school_id)
-    end
+  def new
+    @advert = @parent.advert.new
+  end
+
+  def create
+    @advert = @parent.advert.new(advert_params)
+    @advert.save
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+private
+
+  def set_advert
+    @advert = Advert.find(params[:id])
+  end
+
+  def set_parent
+    @parent = Parent.find(params[:parent_id])
+  end
+
+  def set_school
+    @school = School.find(params[:school_id])
+  end
+
+  def advert_params
+    params.require(:advert).permit(:title, :description, :category, :transaction, :price_cents, :published, :sold, :parent_id, :school_id)
+  end
+
 end
+
 
--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,7 +1,17 @@
 class Advert < ActiveRecord::Base
+  CATEGORYS = [ 'classe', 'jeux', 'fête', 'garde', 'bébé', 'urgence', 'anglais', 'maison', 'zen', 'bons_plans']
+  TRANSACTIONS = [ 'je vends', 'je prête', 'je donne']
   belongs_to :parent
   belongs_to :school
+  has_many :camps, through: :schools
   monetize :price_cents
 
-  validates_presence_of :parent, :school, :price_cents
+  validates_presence_of :parent, :school, :price_cents, :title, :category, :transaction, :description, :parent_id, :school_id
+  validates_inclusion_of :category, in: CATEGORYS
+  validates_inclusion_of :transaction, in: TRANSACTIONS
+
+  def display_name
+    "#{advert.title} - #{advert.category} - #{advert.transaction} - #{school.name}"
+  end
 end
+
