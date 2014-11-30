8a1553a22f1ce277f51c1d431cdf3706b5fe0551
Annabel <annabel.menard@gmail.com>
2014-11-30 15:01:33 +0100

    add school controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
 
 
+  get 'schools/new'
+
+  get 'schools/create'
+
+  get 'schools/update'
+
+  get 'schools/edit'
+
+  get 'schools/destroy'
+
+  get 'schools/index'
+
+  get 'schools/show'
+
   ActiveAdmin.routes(self)
 
   devise_for :parents
