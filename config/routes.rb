c52bba8364af11aaf2bef9bb4df064e00cd3bad2
CandiceSG <candice.salagasperini@gmail.com>
2014-11-30 15:52:45 +0100

    routes modifiées

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,6 @@
 Rails.application.routes.draw do
 
 
-  get 'events/new'
-
-  get 'events/create'
-
-  get 'events/edit'
-
-  get 'events/update'
-
-  get 'events/index'
-
-  get 'events/show'
-
-  get 'events/destroy'
-
-  get 'guests/destroy'
-
-  get 'guests/new'
-
-  get 'guests/create'
-
-  get 'guests/index'
-
-  get 'guests/show'
-
-  get 'guests/edit'
-
-  get 'guests/update'
-
-  get 'guests/delete'
-
   ActiveAdmin.routes(self)
 
   devise_for :parents
@@ -38,6 +8,10 @@ Rails.application.routes.draw do
 
   resources :camps do
     resources :kids, only: :index
+  end
+
+  resources :events do
+    resources :guests
   end
 
   resources :kids do
