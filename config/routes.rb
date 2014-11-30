b5ee1deea7f25d29d88cdbad5cec970e42b7e3d1
Annabel <annabel.menard@gmail.com>
2014-11-30 15:41:42 +0100

    corrections

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,10 @@
 Rails.application.routes.draw do
 
-
-  get 'schools/new'
-
-  get 'schools/create'
-
-  get 'schools/update'
-
-  get 'schools/edit'
-
-  get 'schools/destroy'
-
-  get 'schools/index'
-
-  get 'schools/show'
-
   ActiveAdmin.routes(self)
 
   devise_for :parents
   root to: "home#index"
+  resources :schools
   resources :camps
   resources :kids do
     resources :parentships
