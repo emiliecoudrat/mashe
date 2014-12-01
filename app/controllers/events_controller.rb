94d9b43e758a93559cea0129dc97d8aea859d552
CandiceSG <candice.salagasperini@gmail.com>
2014-12-01 11:27:00 +0100

    ajout des gems + début methode event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,16 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
   def new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @school_events, notice: "Votre événement a bien été créé."
+    else
+      render :new, notice: "Petit problème... réessayez svp."
+    end
   end
 
   def edit
@@ -19,4 +27,13 @@ class EventsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:name, :description, :starts_at, :end_date)
+    end
 end
