6584e3407d7102b894e898b767ef9ca9ca7b1f80
Caroline Carriere <caroline.carrierek@gmail.com>
2014-12-03 15:37:59 +0100

    picture_loaded

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202165252) do
+ActiveRecord::Schema.define(version: 20141203133838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 20141202165252) do
     t.integer  "school_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "adverts", ["parent_id"], name: "index_adverts_on_parent_id", using: :btree
